123fa426b34d446749febad2a20f055348a3c88d
Lang Man <lang.man@husky.neu.edu>
2018-08-04 15:02:13 -0400

    modify setting

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'chatroom.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 import dj_database_url
 db_from_env = dj_database_url.config()
